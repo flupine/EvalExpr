0b6816d171cc9d560b34c87bd55f5390ede3ea78
Faurest <faurest.lupine@epitech.eu>
2017-10-27 15:03:09 +0200

    expr error fix

--- src/main.c
+++ src/main.c
@@ -1,19 +1,23 @@
 /*
-** main.c for src in /home/flupine/CPool_evalexpr_2017/src
+** main.c for src in /home/flupine/Documents/evalexpr/src
 ** 
 ** Made by Faurest Lupine
 ** Login   <faurest.lupine@epitech.eu>
 ** 
 ** Started on  mer. oct. 25 13:58:58 2017 Faurest Lupine
-** Last update mer. oct. 25 14:01:29 2017 Faurest Lupine
+** Last update ven. oct. 27 15:03:03 2017 Faurest Lupine
 */
+
+#include "include/my.h"
+
+int eval_expr(char *str);
 
 int main(int ac , char **av)
 {
 	if (ac == 2)
 	{
 		my_put_nbr(eval_expr(av[1]));
-		my_putchar(’\n’) ;
+		my_putchar('\n') ;
 		return(0);
 	}
 	return(84);
