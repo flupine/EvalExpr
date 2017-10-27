266813949d117f25a3e9827f7d6e9546f25e2a45
Faurest <faurest.lupine@epitech.eu>
2017-10-27 14:51:18 +0200

    Modulo

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 /*
-** main.c for EvalExpr in /home/flupine/Documents/EvalExpr
+** main.c for src in /home/flupine/CPool_evalexpr_2017/src
 ** 
 ** Made by Faurest Lupine
 ** Login   <faurest.lupine@epitech.eu>
 ** 
-** Started on  mer. oct. 25 14:28:35 2017 Faurest Lupine
-** Last update mer. oct. 25 14:28:37 2017 Faurest Lupine
+** Started on  mer. oct. 25 13:58:58 2017 Faurest Lupine
+** Last update mer. oct. 25 14:01:29 2017 Faurest Lupine
 */
-#include "header.h"
 
-int		main(int ac, char **av)
+int main(int ac , char **av)
 {
-	if (ac > 1)
+	if (ac == 2)
 	{
-		ft_putnbr(eval_expr(av[1]));
-		ft_putchar('\n');
+		my_put_nbr(eval_expr(av[1]));
+		my_putchar(’\n’) ;
+		return(0);
 	}
-	return (0);
+	return(84);
 }
