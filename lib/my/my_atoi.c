f25f56df8bba3957cf01b2ebd0e16fd6fe614aef
Faurest <faurest.lupine@epitech.eu>
2017-10-27 15:07:28 +0200

    Test

---
+++ lib/my/my_atoi.c
@@ -0,0 +1,11 @@
+/*
+** my_atoi.c for my in /home/flupine/Documents/evalexpr/lib/my
+** 
+** Made by Faurest Lupine
+** Login   <faurest.lupine@epitech.eu>
+** 
+** Started on  ven. oct. 27 15:04:56 2017 Faurest Lupine
+** Last update ven. oct. 27 15:06:54 2017 Faurest Lupine
+*/
+
+
